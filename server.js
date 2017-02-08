14ea4ec6c043bc0f08212e5907cc3bdbf0ea0022
ShreeranjaniSK <ranjani.sk@gmail.com>
2017-02-08 13:55:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,9 +94,9 @@ app.get('/:articleName',function(req,res){
 });
 
 var names = [];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){//url: /submit-name/?name=xxxx
 //Get name from  the request
-var name = req.params.name;
+var name = req.query.name;
 names.push(name);
 //JSON Javascript Object Notation
 res.send(JSON.stringify(names));
