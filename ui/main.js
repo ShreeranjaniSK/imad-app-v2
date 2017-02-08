60ffbe73c8beb38e35f56d6bbbd06446f1561d3b
ShreeranjaniSK <ranjani.sk@gmail.com>
2017-02-08 13:00:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,26 +2,26 @@ console.log('Loaded!');
 
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick=function()
 {
   //Create a request the counter endpoint
-  //var request = new XMLHttpRequest();
+  var request = new XMLHttpRequest();
   
   //Capture the response and store in variable
-  //request.onreadystatechange = function()
- // {
-   //   if (request.readystate === XMLHttpRequest.DONE){
+  request.onreadystatechange = function()
+  {
+    if (request.readyState === XMLHttpRequest.DONE){
           //take action
-     //     if(request.status === 200){
-            //  var counter = request.responseText;
-             counter = counter + 1;
+          if(request.status === 200){
+              var counter = request.responseText;
               var span = document.getElementById('count');
               span.innerHTML = counter.toString();
-   //       }
-    //  }
+          }
+      }
       //not done yet
   };
  //Make Request
- //request.open('GET','http://shreeranjanisk.imad.hasura-app.io/counter',true);
- //request.send(null);
+ request.open('GET','http://shreeranjanisk.imad.hasura-app.io/counter',true);
+ request.send(null);
+};
