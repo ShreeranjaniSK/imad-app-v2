9e26978b0bb00560f7208bdf947098f315e0653c
ShreeranjaniSK <ranjani.sk@gmail.com>
2017-03-02 13:32:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/hash/:input', function(req, res){
 app.post('/create-user',function(req,res){
     //username and password
     //JSON content gives username and password
-   var username = rq.body.username;
+   var username = req.body.username;
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password,salt); 
