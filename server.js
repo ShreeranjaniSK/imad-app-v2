b2c9da9e31c73ba66cd46e3c43c433092adaf069
ShreeranjaniSK <ranjani.sk@gmail.com>
2017-03-09 11:48:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.post('/login',function(req,res){
            res.status(500).send(err.toString());
        } else{
            if (result.rows.length === 0){
-               res.send(403).send('Invalid Username/Password ');
+               res.status(403).send('Invalid Username/Password ');
            }
            else{
             //Match the password
