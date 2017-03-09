cc2f2cee3e99e05eeed6592d5acba4348af976c5
ShreeranjaniSK <ranjani.sk@gmail.com>
2017-03-09 12:04:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret :'someRandomsecretvalue',
-    cookie :{maxAge :1000 * 60 * 60 * 24 * 30}
+    cookie :{maxAge :1000 * 60 * 60 * 24 * 30},
+    resave: true,
+    saveUninitialized: true
 }));
 
 var articles ={
